fcdd932ecea04f55f339f057aa5a32a3070ec2d1
Yusup <yusup@softwareseni.com>
2019-04-08 18:33:21 +0700

    add mount ujs

--- app/assets/javascripts/webpack/packs/application.js
+++ app/assets/javascripts/webpack/packs/application.js
@@ -21,3 +21,4 @@ console.log('Hello World from Webpacker')
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
+ReactRailsUJS.mountComponents();
